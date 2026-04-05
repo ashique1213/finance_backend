287cac6f22300282f54e624dbaa2a812dcfa658b
ashique1213 <muhammedashique1213@gmail.com>
2026-04-05 19:10:40 +0530

    Implement ViewSets and Dashboard summary API with role-based access control

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.db.models import Sum
+from records.models import FinancialRecord
+from core.permissions import IsViewerOrHigher
 
-# Create your views here.
+class DashboardSummaryView(APIView):
+    permission_classes = [IsViewerOrHigher]
+
+    def get(self, request):
+        records = FinancialRecord.objects.filter(user=request.user)
+
+        total_income = records.filter(type='income').aggregate(Sum('amount'))['amount__sum'] or 0
+        total_expense = records.filter(type='expense').aggregate(Sum('amount'))['amount__sum'] or 0
+
+        return Response({
+            "total_income": float(total_income),
+            "total_expense": float(total_expense),
+            "net_balance": float(total_income - total_expense),
+            "total_records": records.count(),
+        })
--- records/views.py
+++ records/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import FinancialRecord, Category
+from .serializers import FinancialRecordSerializer, CategorySerializer
+from core.permissions import IsAdminUser, IsAnalystOrAdmin, IsViewerOrHigher
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAdminUser]
+
+class FinancialRecordViewSet(viewsets.ModelViewSet):
+    serializer_class = FinancialRecordSerializer
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['type', 'category', 'date']
+    search_fields = ['description', 'category__name']
+    ordering_fields = ['date', 'amount']
+
+    def get_queryset(self):
+        return FinancialRecord.objects.filter(user=self.request.user)
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsViewerOrHigher()]
+        elif self.action in ['create', 'update', 'partial_update']:
+            return [IsAnalystOrAdmin()]
+        elif self.action == 'destroy':
+            return [IsAdminUser()]
+        return [IsAdminUser()]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
